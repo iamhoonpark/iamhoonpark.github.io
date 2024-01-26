f3cf49a30f8e11525e3593a067b14d50eb418b65
박상훈 <psh995666@gmail.com>
2024-01-26 14:32:27 +0900

    240126 Pop-Up

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
 /* Home - 애니메이션 */
 const animatedText = document.querySelectorAll(".animated-text");
 
-function animate(element){
+function animate(element) {
   const textArray = element.innerText.split("");
   element.firstChild.remove();
   
@@ -22,3 +22,33 @@ function animate(element){
 }
 
 Array.from(animatedText).map(animate)
+
+
+/* PopUp - 애니메이션 */
+const popUp = document.querySelector('.pop-box');
+const target = document.querySelector('#about');
+
+function displayPopUp() {
+  const targetTop = target.getBoundingClientRect().bottom;
+  const windowHeight = window.innerHeight;
+
+  if (targetTop < windowHeight) {
+    popUp.classList.add('show');
+  } else {
+    popUp.classList.remove('show');
+  }
+}
+
+window.addEventListener('scroll', displayPopUp);
+
+displayPopUp();
+
+
+/* PopUp - 닫기버튼 */
+document.querySelector('.close-btn').addEventListener('click', function() {
+  const popBox = document.querySelector('.pop-box');
+  popUp.classList.remove('show');
+  setTimeout(function() {
+      popBox.parentNode.removeChild(popBox);
+  }, 100);
+});
